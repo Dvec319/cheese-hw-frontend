c81d56cf58418e22a920b5375a83a473a2a3ebb2
mdelvecchio <mdelvecchio3119@gmail.com>
2023-09-18 16:45:21 -0400

    added in form clearing on submit

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,18 +1,41 @@
 import Post from '../components/Post';
 import { useLoaderData, Form } from 'react-router-dom';
+import { useState } from 'react';
 
 function Index(props) {
 	// grab the loader data using the loaderdata hook
     const cheeses = useLoaderData()
 
+    const [formData, setFormData] = useState({
+        name: '',
+        country: ''
+    })
+
+    const handleChange = (event) => {
+        const newState = {...formData}
+        newState[event.target.name] = event.target.value
+        setFormData(newState)
+    }
+
+    const handleSubmit = (event => {
+        setFormData({
+            name: '',
+            country: ''
+        })
+    })
+
     // map over the cheeses and display them
     return <>
     <div style={{textAlign: 'center'}}>
         <h1>Create a new cheese!</h1>
-        <Form method='post' action='/create'>
-            <input type='text' name='name' placeholder='Name'/>
-            <input type='text' name='country' placeholder='Country'/>
-            <button>Create the cheese!</button>
+        <Form method='post' action='/create' onSubmit={handleSubmit}>
+            <fieldset>
+                <label for='nameField' style={{textAlign: 'left'}}>Name</label>
+                <input type='text' name='name' placeholder='Name' id='nameField' value={formData.name} onChange={handleChange}/>
+                <label for='countryField' style={{textAlign: 'left'}}>Country</label>
+                <input type='text' name='country' placeholder='Country' id='countryField' value={formData.country} onChange={handleChange}/>
+                <button type='submit'>Create the cheese!</button>
+            </fieldset>
         </Form>
     </div>
     {cheeses.map((cheese) => <Post key={cheese.id} post={cheese}/>)}
